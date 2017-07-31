d7ca9d55100ae0c7a1c2b1ea5ac79646db51fd40
dww_w <18292183200@139.com>
2017-07-31 16:22:53 +0800

    init

---
+++ springboot-server-config/src/main/java/com/ytain/springboot/config/SpringbootServerConfigApplication.java
@@ -0,0 +1,12 @@
+package com.ytain.springboot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServerConfigApplication.class, args);
+	}
+}
