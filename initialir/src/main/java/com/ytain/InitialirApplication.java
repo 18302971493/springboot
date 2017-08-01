03deff42bf9f09e9decbc8c000569da9521742d4
dww_w <18292183200@139.com>
2017-08-01 13:08:40 +0800

    修改配置文件为yml

--- initialir/src/main/java/com/ytain/InitialirApplication.java
+++ initialir/src/main/java/com/ytain/InitialirApplication.java
@@ -9,8 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author dww_w
  *
  */
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class InitialirApplication {
 
 	public static void main(String[] args) {
